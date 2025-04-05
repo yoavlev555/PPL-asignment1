8d76074d86bc60506cdab73a50197e21f8dde7ee
Yoav Levin <yoavlev555@gmail.com>
2025-04-05 17:15:19 +0300

    complete part2

--- template/src/part2/part2.ts
+++ template/src/part2/part2.ts
@@ -2,17 +2,43 @@ import * as R from "ramda";
 
 const stringToArray = R.split("");
 console.log(stringToArray("hello")); // ["h", "e", "l", "l", "o"]
+
 /* Question 1 */
 const vowels: string[] = ['a', 'e', 'i', 'o', 'u'];
-export const countVowels: undefined = undefined;
+
+export const countVowels: (str: string) => number = (str: string) => {
+    const strArray: string[] = stringToArray(str);
+    const filterdArray: string[] = strArray.filter((char: string) => R.toLower(char) in vowels);
+    return filterdArray.length;
+};
 
 /* Question 2 */
-export const isPalindrome: undefined = undefined;
-  
+export const isPalindrome: (str: string) => boolean = (str: string) => {
+    if (str.length === 0 || str.length === 1) {
+        return true;
+    }
+
+    else {
+        // Filter out non-alphabetic characters and convert to lowercase
+        const f = R.pipe(R.toLower, stringToArray, R.filter((char: string) => char.match(/[a-z]/) !== null), R.join(""))
+        const filteredString: string = f(str);
+        return filteredString[0] === filteredString[filteredString.length - 1] && isPalindrome(filteredString.slice(1, filteredString.length - 1));
+    }
+};
+
 /* Question 3 */
 export type WordTree = {
     root: string;
     children: WordTree[];
 }
 
-export const treeToSentence: undefined = undefined;
+export const treeToSentence: (wt: WordTree) => string = (wt: WordTree) => {
+    if (wt.children.length === 0) {
+        return wt.root;
+    }
+
+    else {
+        return wt.root + wt.children.reduce((res: string, element: WordTree) => res += " " + treeToSentence(element), "");
+    }
+};
+
